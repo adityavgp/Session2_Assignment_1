a392a3b653fa6e9972d87f8bcd3b8f8ed5c4ef69
adityavgp <gpaditya11@gmail.com>
2016-09-08 18:46:32 +0530

    Add files via upload

---
+++ A2_1.java
@@ -0,0 +1,10 @@
+package com.a2.classes;
+
+public class A2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (System.out.printf("Aditya VGP") == null) {}
+	}
+
+}
